aa5e6270d31081bf35a39cf1d5cd861ab4c2ecf1
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-02-14 21:28:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,8 +34,8 @@ var ti={}
 ,attentions={};
 
 client.on('ready', function(){
-    var ms = 3 ;
-    var setGame = ['<o/help','Type <o/help','Maker by B','Maker by Be','Maker by Bea','Maker by Bear','Maker by BearB','Maker by BearBo','Maker by BearBoy'];
+    var ms = 60000 ;
+    var setGame = ['<o/help','Type <o/help','Maker by BearBo','Maker by BearBoy'];
     var i = -1;
     var j = 0;
     setInterval(function (){
